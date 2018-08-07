61365748c8a55ebbec9c1ba23117782d5400f2e6
rvd2pwipwip <herve_desrosiers@hotmail.com>
2018-08-06 20:08:50 -0400

    Handling events 1:01:07

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,18 +2,30 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    // tags: ["tag1", "tag2", "tag3"]
+    tags: []
   };
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
+  // formatCount() {
+  //   const { count } = this.state; //destructuring this.state.count
+  //   return count === 0 ? "Zero" : count;
+  // }
+  // getBadgeClasses() {
+  //   let classes = "badge m-2 badge-";
+  //   classes += this.state.count === 0 ? "warning" : "primary";
+  //   return classes;
+  // }
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button> */}
+        {this.state.tags.length ? (
+          <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+        ) : (
+          <p>No tags</p>
+        )}
       </React.Fragment>
     );
   }
