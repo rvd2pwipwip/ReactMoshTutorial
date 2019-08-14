d317e77147f7e793fd9a459d83c750fd800e2d0e
Herve Desrosiers <herve_desrosiers@hotmail.com>
2019-08-13 20:13:50 -0400

    video07 rendering classes dynamically

--- counter-app02/src/components/counter.jsx
+++ counter-app02/src/components/counter.jsx
@@ -3,16 +3,24 @@ import * as React from "react";
 const Counter = () => {
   const [count, setCount] = React.useState(0);
 
+  // useEffect(() = {}, count)
+
   const formatCount = () => {
     return count === 0 ? "Zero" : count;
   };
 
   return (
     <>
-      <span>{formatCount()}</span>
-      <button>Increment</button>
+      <span className={getBadgeClasses(count)}>{formatCount()}</span>
+      <button className="btn btn-secondary btn-sm">+</button>
     </>
   );
 };
 
 export default Counter;
+
+const getBadgeClasses = count => {
+  let classes = "badge m-2 badge-";
+  classes += count === 0 ? "warning" : "primary";
+  return classes;
+};
