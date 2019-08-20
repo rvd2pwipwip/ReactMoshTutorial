3171da6f3df5e0b296e8aef8a583983c82400f42
Herve Desrosiers <herve_desrosiers@hotmail.com>
2019-08-19 20:29:52 -0400

    need to figure out useEffect for table message header

--- rv-vidly/src/components/Movies.jsx
+++ rv-vidly/src/components/Movies.jsx
@@ -2,32 +2,51 @@ import React from "react";
 import { getMovies } from "./../services/fakeMovieService";
 
 const Movies = () => {
-  const [movies] = React.useState(getMovies);
+  const [movies, setMovies] = React.useState(getMovies);
+
+  React.useEffect(() => {});
+
+  const handleDelete = id => {
+    setMovies(movies.filter(m => m._id !== id));
+  };
+
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Genre</th>
-          <th>Stock</th>
-          <th>Rate</th>
-          <th />
-        </tr>
-      </thead>
-      <tbody>
-        {Object.values(movies).map(m => (
-          <tr key={m._id}>
-            <td>{m.title}</td>
-            <td>{m.genre.name}</td>
-            <td>{m.numberInStock}</td>
-            <td>{m.dailyRentalRate}</td>
-            <td>
-              <button className="btn btn-danger btn-sm">Delete</button>
-            </td>
+    <>
+      {movies ? (
+        <p>Showing {movies.length} movies in the database.</p>
+      ) : (
+        <p>There are no movies in the database.</p>
+      )}
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Stock</th>
+            <th>Rate</th>
+            <th />
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {Object.values(movies).map(m => (
+            <tr key={m._id}>
+              <td>{m.title}</td>
+              <td>{m.genre.name}</td>
+              <td>{m.numberInStock}</td>
+              <td>{m.dailyRentalRate}</td>
+              <td>
+                <button
+                  className="btn btn-danger btn-sm"
+                  onClick={() => handleDelete(m._id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
 
